6a8e68ffb8025e0aef664c3228e61c1d00579760
nadjarutsch <nadja.rutsch@gmx.de>
2021-05-15 20:32:27 +0200

    init heuristics

---
+++ heuristics.py
@@ -0,0 +1,43 @@
+import torch
+import numpy as np
+
+HEURISTICS = ['uniform', 'uncertain']
+
+
+def choose_intervention(heuristic: str, gamma: torch.Tensor) -> int:
+    """Chooses a node for intervention.
+    
+    Args:
+        gamma_matrix: Tensor of shape (n_nodes, n_nodes) with gamma-parameters,
+            representing the edge probabilities.
+
+    Returns:
+        Index of the node to be intervened on.
+        
+    Raises:
+        Exception: If heuristic is not available.
+    """    
+    if heuristic == HEURISTICS[0]:
+        return uniform(gamma)
+    
+    elif heuristic == HEURISTICS[1]:
+        return uncertain(gamma)  
+    
+    else:
+        raise Exception('Heuristic is not available. \n Chosen heuristic: {} \n Available heuristics: {}'.format(heuristic, HEURISTICS))
+
+
+def uniform(gamma: torch.Tensor) -> int:
+    """Samples an intervention node uniformly from all nodes."""
+    
+    return np.random.randint(gamma.shape[-1])
+
+
+def uncertain(gamma: torch.Tensor) -> int:
+    """Chooses the intervention node with the most uncertain outgoing edge."""
+   
+    # TODO: check if [0] really returns the node with the most uncertain 
+    # outgoing edge
+    return np.unravel_index(np.argmin(torch.abs(gamma)), gamma.shape)[0]
+    
+
